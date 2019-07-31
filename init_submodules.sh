fdbf8314e683e6ce2327bffbd61930c232775ad9
Marko Kosunen <marko.kosunen@aalto.fi>
2019-07-31 14:10:24 +0300

    Fix init_submodules.sh

---
+++ init_submodules.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#Selective initialization of submodules
+#Written by by Marko Kosunen, marko.kosunen@aalto.fi, 2017
+DIR=$( cd `dirname $0` && pwd )
+SUBMODULES="\
+    ./vhdl_ecd_submodule \
+    "
+
+git submodule sync
+for mod in $SUBMODULES; do 
+    git submodule update --init $mod
+    cd ${mod}
+    if [ -f ./init_submodules.sh ]; then
+        ./init_submodules.sh
+    fi
+    cd ${DIR}
+
+done
+exit 0
+
+
